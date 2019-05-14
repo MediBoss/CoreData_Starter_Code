527a266567712e6bf91ce3571f734277f4970ee0
Medi Assumani <mediassumani49@gmail.com>
2019-05-14 16:11:22 -0700

    [Add] initial commit

---
+++ CoreDataExercise/CoreDataExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataExercise
+//
+//  Created by Medi Assumani on 5/14/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
