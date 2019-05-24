dc798ea062e63b90bea53fbf0dd1f6a81f9db136
Medi Assumani <mediassumani49@gmail.com>
2019-05-23 17:08:27 -0700

    updated

--- ShoppingStores/ShoppingStores/ViewController.swift
+++ ShoppingStores/ShoppingStores/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  CoreDataExercise
+//  ShoppingStores
 //
-//  Created by Medi Assumani on 5/14/19.
+//  Created by Medi Assumani on 5/23/19.
 //  Copyright © 2019 Medi Assumani. All rights reserved.
 //
 
